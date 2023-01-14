58b3f3b0ee9ea694192721472105fb17e73831a2
damir <damir1odo@gmail.com>
2023-01-14 21:20:20 +0300

    change feedback

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,9 +20,9 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
+import feedbackLogo from "./feedbackLogo.png";
+import feedbackLogo2 from "./feedbackLogo_2.jpg";
+import feedbackLogo3 from "./feedbackLogo_3.jpg";
 
 export {
   airbnb,
@@ -47,7 +47,7 @@ export {
   instagram,
   linkedin,
   twitter,
-  people01,
-  people02,
-  people03,
+  feedbackLogo,
+  feedbackLogo2,
+  feedbackLogo3,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+import { feedbackLogo, feedbackLogo2, feedbackLogo3, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
 
 export const navLinks = [
   {
@@ -48,25 +48,25 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+    name: "Jesse Bruce Pinkman",
     title: "Founder & Leader",
-    img: people01,
+    img: feedbackLogo,
   },
   {
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+    name: " Walter Hartwell White",
     title: "Founder & Leader",
-    img: people02,
+    img: feedbackLogo2,
   },
   {
     id: "feedback-3",
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+    name: "Gustavo Fring",
     title: "Founder & Leader",
-    img: people03,
+    img: feedbackLogo3,
   },
 ];
 
